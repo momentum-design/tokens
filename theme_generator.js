77663d6c8d740d44fa1d9ead35738642708424b3
Paul Witty <pauwitty@cisco.com>
2021-06-04 15:37:10 +0100

    Write generated files to themes; provide options to configure the output

--- theme_generator.js
+++ theme_generator.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
-var parseColour = require('color-parse');
-var merge = require('lodash.merge');
+const parseColour = require('color-parse');
+const merge = require('lodash.merge');
+const args = require('args-parser')(process.argv);
+const camelCase = require('camelcase');
 
 function loadFile(fileName, isDirectory) {
   if (isDirectory) {
@@ -61,27 +63,44 @@ function resolveValue(key, value, tokens, coretokens, breadcrumbs) {
   }
 }
 
+let platform='Web';
+if (args.platform) {
+  platform = args.platform;
+  delete args.platform;
+}
 
+let colorFormat='rgba';
+if (args.colorFormat) {
+  if (args.colorFormat == 'hex') {
+    colorFormat = 'hex';
+  } else if (args.colorFormat == 'rgba') {
+    colorFormat = 'rgba';
+  } else {
+    console.log('Unknown color format: ' + args.colorFormat);
+    process.exit(1);
+  }
+  delete args.colorFormat;
+}
+
+let toStdOut=false;
+if (args.toStdOut) {
+  toStdOut = args.toStdOut;
+  delete args.toStdOut;
+}
+
+if (Object.keys(args).length == 0) {
+  console.log(`Usage: ${process.argv[1]} [OPTION]... [THEME FILE]...`);
+  console.log('Options');
+  console.log('  --colorFormat=[hex|rgba]    What color format to use in the output.');
+  console.log('       rgba -> rgba(244,233,20,0.8)');
+  console.log('       hex  -> #RRGGBBAA (or would be if someone fixes it)');
+  console.log('  --platform=PLATFORM         Which platform to generate for.');
+  process.exit(1);
+}
 
 // Start by loading all the token files
+console.log('=== Loading core files ===================');
 let coreData = loadFile("core", true);
-//console.log(coreData);
-
-let themeFileData = fs.readFileSync(process.argv[2]);
-let themeFile = JSON.parse(themeFileData);
-console.log('Loading theme ' + themeFile.name);
-let tokenData = {};
-themeFile.files.forEach((fileName) => {
-  if (fileName.endsWith('/')) {
-    merge(tokenData, loadFile(fileName.slice(0, -1), true));
-  } else {
-    merge(tokenData, loadFile(fileName, false));
-  }
-});
-//console.log(tokenData);
-
-
-
 // Then flatten all the tokens
 const flattenedCoreTokens = {};
 flattenObject('', coreData, flattenedCoreTokens);
@@ -89,37 +108,85 @@ flattenObject('', coreData, flattenedCoreTokens);
 Object.entries(flattenedCoreTokens).forEach(([key, value]) => {
   if ((typeof value === 'string') && (value.startsWith('#') || value.startsWith('rgb'))) {
     const c = parseColour(value);
-    flattenedCoreTokens[key] = `rgba(${c.values[0]}, ${c.values[1]}, ${c.values[2]}, ${c.alpha})`;
+    if (!c) {
+      console.log('Unable to parse colour' + value);
+      process.exit(1);
+    }
+    if (colorFormat == 'rgba') {
+      flattenedCoreTokens[key] = `rgba(${c.values[0]}, ${c.values[1]}, ${c.values[2]}, ${c.alpha})`;
+    } else if (colorFormat == 'hex') {
+      flattenedCoreTokens[key] = ((c.values[0] << 24) | (c.values[1] << 16) | (c.values[2] << 8) | c.alpha).toString(16);
+    }
   }
 });
-const flattenedTokens = {};
-flattenObject('', tokenData, flattenedTokens);
-//console.log(flattenedCoreTokens);
-//console.log(flattenedTokens);
+
+console.log('=== Core files loaded ====================');
+
+Object.keys(args).forEach(themeFileName => {
+  console.log('=== Processing theme =====================');
+  let themeFileData = fs.readFileSync(themeFileName);
+  let themeFile = JSON.parse(themeFileData);
+  console.log(`Loading theme ${themeFile.name} for platform ${platform} using color format ${colorFormat}`);
+  let tokenData = {};
+  themeFile.files.forEach((fileName) => {
+    if (fileName.endsWith('/')) {
+      merge(tokenData, loadFile(fileName.slice(0, -1), true));
+    } else {
+      merge(tokenData, loadFile(fileName, false));
+    }
+  });
+  //console.log(tokenData);
 
 
 
-// Then resolve all the tokens
-Object.entries(flattenedTokens).forEach(([key, value]) => {
-  flattenedTokens[key] = resolveValue(key, value, flattenedTokens, flattenedCoreTokens, []);
-});
-//console.log(flattenedTokens);
+  const flattenedTokens = {};
+  flattenObject('', tokenData, flattenedTokens);
+  //console.log(flattenedCoreTokens);
+  //console.log(flattenedTokens);
 
 
 
-// And then expand every token into UI states
-const stateTokens = {};
-Object.entries(flattenedTokens).forEach(([key, value]) => {
-  const keyParts = key.split('-');
-  let uiState = 'normal';
-  if (keyParts[keyParts.length-1].startsWith('#')) 
-  {
-    uiState = keyParts.pop().slice(1);
+  // Then resolve all the tokens
+  Object.entries(flattenedTokens).forEach(([key, value]) => {
+    flattenedTokens[key] = resolveValue(key, value, flattenedTokens, flattenedCoreTokens, []);
+  });
+  //console.log(flattenedTokens);
+
+
+
+  // And then expand every token into UI states
+  const stateTokens = {};
+  Object.entries(flattenedTokens).forEach(([key, value]) => {
+    const keyParts = key.split('-');
+    let uiState = 'normal';
+    if (keyParts[keyParts.length-1].startsWith('#')) 
+    {
+      //This is a state, so sanity check it falls into allowed values
+      uiState = keyParts.pop().slice(1);
+      const validUiStates = ['normal', 'hovered', 'pressed', 'disabled', 'focused', 'active', 'checked'];
+      if (!validUiStates.includes(uiState)) {
+        console.log(`Unknown ui state: ${uiState} when resolving ${key}`);
+        process.exit(1);
+      }
+    }
+    const baseKeyName = keyParts.join('-');
+    if (!(baseKeyName in stateTokens)) {
+      stateTokens[baseKeyName] = {};
+    }
+    stateTokens[baseKeyName][uiState] = value;
+  });
+  if (toStdOut) {
+    console.log(stateTokens);
+  } else {
+    const outputFileName = camelCase(themeFile.name) + '.json';
+    fs.writeFile(outputFileName, JSON.stringify(stateTokens, null, 2), 'utf8', function (err) {
+      if (err) 
+      {
+        console.log("Error when writing JSON file: " + err);
+        process.exit(1);
+      }
+    });
+    console.log(`Written to ${outputFileName}`);
   }
-  const baseKeyName = keyParts.join('-');
-  if (!(baseKeyName in stateTokens)) {
-    stateTokens[baseKeyName] = {};
-  }
-  stateTokens[baseKeyName][uiState] = value;
+  console.log('=== Theme processed ======================');
 });
-console.log(stateTokens);
