b2bfdb68e746019eb98d5f639188523a27bd3cba
Paul Witty <pauwitty@cisco.com>
2021-09-06 20:42:51 +0100

    Make high contrast work on other platforms

--- theme_generator.js
+++ theme_generator.js
@@ -11,7 +11,7 @@ function useColourNames(tokens, path) {
     if (typeof value === "object") {
       useColourNames(value, colourName);
     } else if (typeof value === "string") {
-      tokens[key] = colourName;
+      tokens[key] = colourName.replace("SystemColor","").replace("hc-",""); // We modify colour names for high contrast;
     } else {
       console.error("Unexpected type of colour " + typeof value);
       process.exit(1);
@@ -264,7 +264,7 @@ function resolveValue(currentToken, allTokens, coreTokens, flattenedCoreTokens) 
     }
   } else {
     // Otherwise, we don't need to do any resolution
-    return winSystemHighContrast ? normalizeWinHCToken(currentToken) : currentToken;
+    return currentToken;
   }
 }
 
@@ -339,34 +339,6 @@ function normaliseUnits(tokens) {
   return normalisedTokens;
 }
 
-function normalizeWinHCToken(token) {
-  const allowedTokens = new Set([
-    "SystemColorWindowTextColor",
-    "SystemColorHotlightColor",
-    "SystemColorGrayTextColor",
-    "SystemColorHighlightTextColor",
-    "SystemColorHighlightColor",
-    "SystemColorButtonTextColor",
-    "SystemColorButtonFaceColor",
-    "SystemColorWindowColor",
-    "WindowTextColor",
-    "HotlightColor",
-    "GrayTextColor",
-    "HighlightTextColor",
-    "HighlightColor",
-    "ButtonTextColor",
-    "ButtonFaceColor",
-    "WindowColor",
-    "PlaceholderColor"]);
-
-  if (!allowedTokens.has(token)) {
-    console.error("Unable to find " + token + " in the set of allowed tokens for Windows high-contrast");
-    process.exit(1);
-  }
-
-  return token.replace(/^(SystemColor)/,"");
-}
-
 let platform = "web";
 if (args.platform) {
   platform = args.platform;
@@ -382,7 +354,6 @@ let includeMobileTokens = false;
 let includeDesktopTokens = false;
 let uiStatesAsObject = true;
 let omitThemeTokens = false;
-let winSystemHighContrast = false;
 
 /* if true we use the background colour as the value of the border if border=none
  * if false, we instead modify to a border-style and border-color variables
@@ -463,11 +434,6 @@ if (args.omitThemeTokens) {
   delete args.omitThemeTokens;
 }
 
-if (args.winSystemHighContrast) {
-  winSystemHighContrast = args.winSystemHighContrast;
-  delete args.winSystemHighContrast;
-}
-
 if (args.fileFormat) {
   if (args.fileFormat === "css") {
     fileFormat = "css";
@@ -509,7 +475,6 @@ if (Object.keys(args).length === 0) {
   console.log("       rem   -> root em, used on web to create sizes relative to user font size");
   console.log("  --componentGroups           Group tokens by component");
   console.log("  --omitThemeTokens           Removes theme tokens from the generated file");
-  console.log("  --winSystemHighContrast     Generate a Windows system high-contrast theme, will only be resolved to actual colors at app runtime");
   console.log("  --fileFormat=[css|json]     What format to use for the output files");
   console.log("  --platform=PLATFORM         Which platform to generate for.");
   console.log("       web");
@@ -523,14 +488,10 @@ if (Object.keys(args).length === 0) {
 
 // Start by loading all the token files
 let coreTokens = []
-if (!winSystemHighContrast) {
-  console.log("=== Loading core files ===================");
-  coreTokens = loadFile("core", true);
-  if (colorFormat === "names") {
-    useColourNames(coreTokens);
-  }
-} else {
-  console.log("=== Skipping loading of core files, system high-contrast theme selected")
+console.log("=== Loading core files ===================");
+coreTokens = loadFile("core", true);
+if (colorFormat === "names") {
+  useColourNames(coreTokens);
 }
 
 // Then flatten all the tokens
@@ -574,7 +535,7 @@ Object.keys(args).forEach((themeFileName) => {
   // Load all the files to build one big object
   let themeFileData = fs.readFileSync(themeFileName);
   let themeFile = JSON.parse(themeFileData);
-  console.log(`Loading theme ${themeFile.name} for platform ${platform} using color format ${colorFormat}`);
+  console.log(`Loading theme ${themeFile.accent} ${themeFile.theme} for platform ${platform} using color format ${colorFormat}`);
 
   const themeData = {};
   themeFile.files.forEach((fileName) => {
