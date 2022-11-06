560592e7aa2ea11eaa7f7f654ab4c1c70bf616d3
Christopher DuBois <chdubois@cisco.com>
2022-11-06 15:35:12 -0800

    fix(scripts): pass args

--- scripts/index.js
+++ scripts/index.js
@@ -2,14 +2,17 @@ const args = require("args-parser")(process.argv);
 
 const commands = require("./commands");
 
-const executeCommand = (command) => {
+const executeCommand = (command, format) => {
+  if (!["automated", "automated", "design"].includes(format)) {
+    throw new Error(`index.executeCommand() :: format "${format}" not supported, exiting`);
+  }
   if (commands[command]) {
-    commands[command]();
+    commands[command](format);
   } else {
     throw new Error(`index.executeCommand() :: command "${command}" not supported, exiting`);
   }
 };
 
 if (args.command) {
-  executeCommand(args.command);
+  executeCommand(args.command, args.format);
 }
